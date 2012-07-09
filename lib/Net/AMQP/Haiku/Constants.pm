704959bf0baa64b8e31b2ee10b74c7904a70afea
Rebs Guarina <rguarina@wetafx.co.nz>
2012-07-09 12:26:24 +1200

    Net/AMQP/Haiku/Constants.pm:
    * changed DEFAULT_EXCHANGE to DEFAULT_EXCHANGE_NAME
    * Added
    > * DEFAULT_EXCHANGE_TYPE
    > * $EXCHANGE_TYPES

--- lib/Net/AMQP/Haiku/Constants.pm
+++ lib/Net/AMQP/Haiku/Constants.pm
@@ -1,18 +1,24 @@
 package Net::AMQP::Haiku::Constants;
-use strict;
 
+use strict;
+use warnings;
 require Exporter;
-our @ISA    = qw(Exporter);
-our @EXPORT = qw(DEFAULT_HOST DEFAULT_PROTO DEFAULT_PORT DEFAULT_VHOST
-    HANDSHAKE_CHANNEL DEFAULT_CHANNEL DEFAULT_QUEUE
-    DEFAULT_USERNAME DEFAULT_PASSWORD FLAG_DEBUG
-    DEFAULT_AUTH_MECHANISM $AUTH_MECHANISM_LIST CLIENT_PLATFORM
-    DEFAULT_RECV_LEN SEND_RETRY RECV_RETRY DEFAULT_TIMEOUT DEFAULT_EXCHANGE
-    _FOOTER_OCT _FOOTER_LENGTH _HEADER_LENGTH DEFAULT_TICKET
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+    DEFAULT_HOST DEFAULT_PORT DEFAULT_PROTO DEFAULT_TIMEOUT
+    DEFAULT_LOCALE DEFAULT_AUTH_MECHANISM
+    DEFAULT_USERNAME DEFAULT_PASSWORD
+    DEFAULT_VHOST DEFAULT_QUEUE DEFAULT_CHANNEL HANDSHAKE_CHANNEL
+    CLIENT_PLATFORM FLAG_DEBUG
+    SEND_RETRY RECV_RETRY DEFAULT_RECV_LEN
+    _FOOTER_OCT _FOOTER_LENGTH _HEADER_LENGTH
+    DEFAULT_TICKET  DEFAULT_EXCHANGE_NAME DEFAULT_EXCHANGE_TYPE
     DEFAULT_PUBLISH_WEIGHT FLAG_MANDATORY FLAG_IMMEDIATE FLAG_NO_ACK
     DEFAULT_CORRELATION_ID DEFAULT_CONSUMER_TAG FLAG_DELIVERY FLAG_PRIORITY
-    DEFAULT_LOCALE FLAG_AUTO_DELETE FLAG_DURABLE FLAG_NO_WAIT FLAG_EXCLUSIVE
-    FLAG_PASSIVE @PUBLISH_FRAME_ATTRS @HEADER_FRAME_ATTRS FLAG_NO_LOCAL);
+    FLAG_AUTO_DELETE FLAG_DURABLE FLAG_NO_WAIT FLAG_EXCLUSIVE FLAG_PASSIVE
+    @PUBLISH_FRAME_ATTRS @HEADER_FRAME_ATTRS FLAG_NO_LOCAL FLAG_INTERNAL
+    $AUTH_MECHANISM_LIST $EXCHANGE_TYPES
+);
 
 ### CONSTANTS ###
 
@@ -43,22 +49,28 @@ use constant _FOOTER_LENGTH => 1;
 use constant _HEADER_LENGTH => 7;
 
 # FRAME stuff
-use constant DEFAULT_TICKET => 0;          # ticket access
-use constant DEFAULT_EXCHANGE => '';    # the name of the exchange you want to use
+use constant DEFAULT_TICKET        => 0;          # ticket access
+use constant DEFAULT_EXCHANGE_TYPE => 'direct';
+use constant DEFAULT_EXCHANGE_NAME =>
+    '';    # the name of the exchange you want to use
 use constant DEFAULT_PUBLISH_WEIGHT => 0;    # frame weight
 use constant FLAG_MANDATORY         => 0;    # mandatory routing
-use constant FLAG_IMMEDIATE         => 0; # for flagging immediate delivery. if set to 1, it will guarantee that the message can be consumed.
+use constant FLAG_IMMEDIATE         => 0
+    ; # for flagging immediate delivery. if set to 1, it will guarantee that the message can be consumed.
 use constant FLAG_DELIVERY => 1;    # for flagging delivery
 use constant FLAG_PRIORITY => 1;    # Basic::ContentHeader priority flag
-use constant FLAG_PASSIVE  => 0; # Exchange::Declare passive flag. If set, it will instruct the server not to create the exchange
+use constant FLAG_PASSIVE  => 0
+    ; # Exchange::Declare passive flag. If set, it will instruct the server not to create the exchange
 use constant FLAG_NO_ACK    => 1;    # frame ack
 use constant FLAG_EXCLUSIVE => 0;    # request exclusive consumer access
-use constant FLAG_DURABLE => 0;    # set queue as durable. Let the mnesia db bring it back on reboot
+use constant FLAG_DURABLE =>
+    0;    # set queue as durable. Let the mnesia db bring it back on reboot
 use constant FLAG_NO_WAIT     => 0;  # tells the server not to reply to method
 use constant FLAG_NO_LOCAL    => 0;  # no_local flag
 use constant FLAG_AUTO_DELETE => 0;  # automaticaly delete the message
 use constant DEFAULT_CONSUMER_TAG   => '';     # default consumer tag
 use constant DEFAULT_CORRELATION_ID => '1';    #
+use constant FLAG_INTERNAL          => 0;
 ### CONSTANTS ###
 
 # frame attributes
@@ -71,6 +83,12 @@ our $AUTH_MECHANISM_LIST = {
     'PLAIN'          => 1,
     'AMQPLAIN'       => 1,
     'RABBIT-CR-DEMO' => 1,
+};
+
+our $EXCHANGE_TYPES = {
+    'direct' => 1,
+    'fanout' => 1,
+    'topic'  => 1,
 };
 
 1;
